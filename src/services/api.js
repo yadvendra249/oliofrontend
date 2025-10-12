ba05b127f571d98d1f97ce687256602b4cd124c3
aditya <adityaadarsh1416@gmail.com>
2025-10-12 22:03:24 +0530

    fixed

--- src/services/api.js
+++ src/services/api.js
@@ -11,6 +11,7 @@ const api = axios.create({
 api.interceptors.request.use(
   (config) => {
     const token = getToken();
+    console.log(tok)
     if (token) {
       config.headers["Authorization"] = `Bearer ${token}`;
     }
